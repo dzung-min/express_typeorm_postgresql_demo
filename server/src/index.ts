a1be3a7025fc4b41c2f07d85107577c7111bf454
Dzung Nguyen <dzung_min@yahoo.com>
2022-02-16 02:59:37 +0700

    using enviroment variables instead of hard coding some sensitive info

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,36 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 import http from 'http'
+import dotenv from 'dotenv'
+dotenv.config()
+
 import app from './app'
 
-const port = process.env.PORT || 5000
+const { PORT, DB_PORT, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } =
+  process.env
 const server = http.createServer(app)
 
-createConnection()
+createConnection({
+  type: 'postgres',
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+})
   .then(async (connection) => {
-    server.listen(port, () => {
-      console.log(`Server is running on port ${port}`)
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
     })
   })
   .catch((error) => console.log(error))
